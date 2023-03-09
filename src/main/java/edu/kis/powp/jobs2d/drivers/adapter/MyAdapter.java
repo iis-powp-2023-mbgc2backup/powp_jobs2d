83f8b69f782358728523b652df15cb970e9ec63a
student <xd@wp.pl>
2023-03-09 13:40:29 +0100

    fix: drawing line on correct window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,6 +4,8 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
 
 /**
  * driver adapter to drawer with several bugs.
@@ -29,7 +31,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		this.startX = x;
 		this.startY = y;
 
-		drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
