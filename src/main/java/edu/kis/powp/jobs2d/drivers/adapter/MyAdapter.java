a64bce6bb703b964d115280550f617c380804fa2
moginski01 <235936@edu.p.lodz.pl>
2023-03-09 13:38:06 +0100

    2.moving result to application from another window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -4,13 +4,14 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class MyAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
-
+	DrawerFeature drawerFeature;
 	public MyAdapter() {
 		super();
 	}
@@ -30,7 +31,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		this.startY=y;
 		this.startX=x;
 
-		drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
