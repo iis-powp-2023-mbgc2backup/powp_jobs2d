4439e8ebaf7c2d13f56b1179b565d1c649bdea69
DMisztal <dominikmisztal@onet.eu>
2023-03-14 13:30:03 +0100

    Figure now properly displays inside of the designed area

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -39,7 +39,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.addDriver("Logger Driver", loggerDriver);
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
-		Job2dDriver testDriver = new Jobs2DToDrawerAdapter();
+		Job2dDriver testDriver = new Jobs2DToDrawerAdapter(application.getFreePanel());
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
 
 		DriverFeature.updateDriverInfo();
