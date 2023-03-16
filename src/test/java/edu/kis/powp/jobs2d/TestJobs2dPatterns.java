9e47d47e2d9c8bc4abaa45c77d981596bb5bb426
MikolajRajczyk <235958@edu.p.lodz.pl>
2023-03-16 13:18:07 +0100

    Wywołanie metod rysujących poprzez wyrażenie lambda

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,21 +10,20 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverToDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
-import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class TestJobs2dPatterns {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-
 	/**
 	 * Setup test concerning preset figures in context.
 	 * 
 	 * @param application Application context.
 	 */
 	private static void setupPresetTests(Application application) {
-		application.addTest("Figure Joe 1", new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 1));
-		application.addTest("Figure Joe 2", new SelectTestFigureOptionListener(DriverFeature.getDriverManager(), 2));
+		application.addTest("Figure Joe 1", (ActionEvent e) -> FiguresJoe.figureScript1(DriverFeature.getDriverManager().getCurrentDriver()));
+		application.addTest("Figure Joe 2", (ActionEvent e) -> FiguresJoe.figureScript2(DriverFeature.getDriverManager().getCurrentDriver()));
 	}
 
 	/**
