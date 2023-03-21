2dc895f3833a25230b5bd8b31f123c018f272ee2
Alt-az <235979@edu.p.lodz.pl>
2023-03-21 12:39:18 +0100

    Poprawka toString w adapterach

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -32,6 +32,6 @@ public class LineDrawerAdapter extends DrawPanelController implements Job2dDrive
 
     @Override
     public String toString() {
-        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+        return "LineDrawerAdapter";
     }
 }
