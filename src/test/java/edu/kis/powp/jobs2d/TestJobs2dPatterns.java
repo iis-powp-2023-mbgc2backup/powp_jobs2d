372b51a74c5e56137487809dd0e29e1453acbdb7
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-03-21 12:44:49 +0100

    Dodanie drugiego testu

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -11,6 +11,7 @@ import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
+import edu.kis.powp.jobs2d.events.TestFigure2;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
@@ -27,6 +28,10 @@ public class TestJobs2dPatterns {
 				DriverFeature.getDriverManager());
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		
+                TestFigure2 testFigure2 = new TestFigure2(
+                                DriverFeature.getDriverManager());
+                application.addTest("Figure Joe 2", testFigure2);
 	}
 
 	/**
