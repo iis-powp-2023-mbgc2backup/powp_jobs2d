05860ce74a4668322d70c35ba6bcaa8d9974f351
Bartosz Humel <235876@edu.p.lodz.pl>
2023-03-21 13:40:22 +0100

    Reformat kodu i dodanie dependencies injection

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -4,53 +4,51 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;
+    private final DrawPanelController dpc;
+    private int startX = 0, startY = 0;
+    private Modes mode = Modes.BASIC;
 
-	private Modes mode=Modes.BASIC;
-
-	public LineDrawerAdapter() {
-		super();
-	}
-
+    public LineDrawerAdapter(DrawPanelController dpc) {
+        super();
+        this.dpc = dpc;
+    }
 
 
-	public void setMode(Modes mode) {
-		this.mode = mode;
-	}
+    public void setMode(Modes mode) {
+        this.mode = mode;
+    }
 
-	@Override
-	public void setPosition(int x, int y) {
-		this.startX = x;
-		this.startY = y;
-	}
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
 
-	@Override
-	public void operateTo(int x, int y) {
-		ILine line;
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line;
 
-		if(mode==Modes.DOTTED)
-		{
-			line = LineFactory.getDottedLine();
-		}else if(mode==Modes.SPECIAL){
-			line = LineFactory.getSpecialLine();
-		}else{
-			line = LineFactory.getBasicLine();
-		}
-		line.setStartCoordinates(this.startX, this.startY);
-		line.setEndCoordinates(x, y);
-		this.startX = x;
-		this.startY = y;
-		DrawerFeature.getDrawerController().drawLine(line);
-	}
+        if (mode == Modes.DOTTED) {
+            line = LineFactory.getDottedLine();
+        } else if (mode == Modes.SPECIAL) {
+            line = LineFactory.getSpecialLine();
+        } else {
+            line = LineFactory.getBasicLine();
+        }
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        this.startX = x;
+        this.startY = y;
+        dpc.drawLine(line);
+    }
 
-	@Override
-	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
-	}
+    @Override
+    public String toString() {
+        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+    }
 }
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -1,10 +1,5 @@
 package edu.kis.powp.jobs2d;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
@@ -17,96 +12,101 @@ import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TestJobs2dPatterns {
-	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-	/**
-	 * Setup test concerning preset figures in context.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupPresetTests(Application application) {
-		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
-				DriverFeature.getDriverManager());
-		SelectSecondTestFigureOptionListener selectSecondTestFigureOptionListener = new SelectSecondTestFigureOptionListener(
-				DriverFeature.getDriverManager());
-		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
-		application.addTest("Figure Joe 2",selectSecondTestFigureOptionListener);
-	}
+    /**
+     * Setup test concerning preset figures in context.
+     *
+     * @param application Application context.
+     */
+    private static void setupPresetTests(Application application) {
+        SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
+                DriverFeature.getDriverManager());
+        SelectSecondTestFigureOptionListener selectSecondTestFigureOptionListener = new SelectSecondTestFigureOptionListener(
+                DriverFeature.getDriverManager());
+        application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+        application.addTest("Figure Joe 2", selectSecondTestFigureOptionListener);
+    }
 
-	/**
-	 * Setup driver manager, and set default driver for application.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupDrivers(Application application) {
-		Job2dDriver loggerDriver = new LoggerDriver();
-		DriverFeature.addDriver("Logger Driver", loggerDriver);
+    /**
+     * Setup driver manager, and set default driver for application.
+     *
+     * @param application Application context.
+     */
+    private static void setupDrivers(Application application) {
+        Job2dDriver loggerDriver = new LoggerDriver();
+        DriverFeature.addDriver("Logger Driver", loggerDriver);
 
-		Job2dDriver testDriver = new Draw2dDriver();
-		DriverFeature.addDriver("Buggy Simulator", testDriver);
+        Job2dDriver testDriver = new Draw2dDriver(DrawerFeature.getDrawerController());
+        DriverFeature.addDriver("Buggy Simulator", testDriver);
 
-		LineDrawerAdapter Dotedline = new LineDrawerAdapter();
-		DriverFeature.addDriver("Dotedline Simulator", Dotedline);
-		Dotedline.setMode(Modes.DOTTED);
+        LineDrawerAdapter Dotedline = new LineDrawerAdapter(DrawerFeature.getDrawerController());
+        DriverFeature.addDriver("Dotedline Simulator", Dotedline);
+        Dotedline.setMode(Modes.DOTTED);
 
-		LineDrawerAdapter Specialline = new LineDrawerAdapter();
-		DriverFeature.addDriver("Specialline Simulator", Specialline);
-		Specialline.setMode(Modes.SPECIAL);
+        LineDrawerAdapter Specialline = new LineDrawerAdapter(DrawerFeature.getDrawerController());
+        DriverFeature.addDriver("Specialline Simulator", Specialline);
+        Specialline.setMode(Modes.SPECIAL);
 
-		DriverFeature.getDriverManager().setCurrentDriver(Dotedline);
+        DriverFeature.getDriverManager().setCurrentDriver(Dotedline);
 
-		DriverFeature.updateDriverInfo();
-	}
+        DriverFeature.updateDriverInfo();
+    }
 
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupDefaultDrawerVisibilityManagement(Application application) {
-		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-		application.addJFrameWindow("Drawer", defaultDrawerWindow);
-		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
-				new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-		defaultDrawerWindow.setVisible(false);
-	}
+    /**
+     * Auxiliary routines to enable using Buggy Simulator.
+     *
+     * @param application Application context.
+     */
+    private static void setupDefaultDrawerVisibilityManagement(Application application) {
+        DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
+        application.addJFrameWindow("Drawer", defaultDrawerWindow);
+        application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
+                new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
+        defaultDrawerWindow.setVisible(false);
+    }
 
-	/**
-	 * Setup menu for adjusting logging settings.
-	 * 
-	 * @param application Application context.
-	 */
-	private static void setupLogger(Application application) {
-		application.addComponentMenu(Logger.class, "Logger", 0);
-		application.addComponentMenuElement(Logger.class, "Clear log",
-				(ActionEvent e) -> application.flushLoggerOutput());
-		application.addComponentMenuElement(Logger.class, "Fine level", (ActionEvent e) -> logger.setLevel(Level.FINE));
-		application.addComponentMenuElement(Logger.class, "Info level", (ActionEvent e) -> logger.setLevel(Level.INFO));
-		application.addComponentMenuElement(Logger.class, "Warning level",
-				(ActionEvent e) -> logger.setLevel(Level.WARNING));
-		application.addComponentMenuElement(Logger.class, "Severe level",
-				(ActionEvent e) -> logger.setLevel(Level.SEVERE));
-		application.addComponentMenuElement(Logger.class, "OFF logging", (ActionEvent e) -> logger.setLevel(Level.OFF));
-	}
+    /**
+     * Setup menu for adjusting logging settings.
+     *
+     * @param application Application context.
+     */
+    private static void setupLogger(Application application) {
+        application.addComponentMenu(Logger.class, "Logger", 0);
+        application.addComponentMenuElement(Logger.class, "Clear log",
+                (ActionEvent e) -> application.flushLoggerOutput());
+        application.addComponentMenuElement(Logger.class, "Fine level", (ActionEvent e) -> logger.setLevel(Level.FINE));
+        application.addComponentMenuElement(Logger.class, "Info level", (ActionEvent e) -> logger.setLevel(Level.INFO));
+        application.addComponentMenuElement(Logger.class, "Warning level",
+                (ActionEvent e) -> logger.setLevel(Level.WARNING));
+        application.addComponentMenuElement(Logger.class, "Severe level",
+                (ActionEvent e) -> logger.setLevel(Level.SEVERE));
+        application.addComponentMenuElement(Logger.class, "OFF logging", (ActionEvent e) -> logger.setLevel(Level.OFF));
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				Application app = new Application("2d jobs Visio");
-				DrawerFeature.setupDrawerPlugin(app);
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Application app = new Application("2d jobs Visio");
+                DrawerFeature.setupDrawerPlugin(app);
 
-				DriverFeature.setupDriverPlugin(app);
-				setupDrivers(app);
-				setupPresetTests(app);
-				setupLogger(app);
+                DriverFeature.setupDriverPlugin(app);
+                setupDrivers(app);
+                setupPresetTests(app);
+                setupLogger(app);
 
-				app.setVisibility(true);
-			}
-		});
-	}
+                app.setVisibility(true);
+            }
+        });
+    }
 
 }
