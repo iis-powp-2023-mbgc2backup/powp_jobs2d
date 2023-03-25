c902a992f5ba06a8399387d53849468a052f2ea6
Alt-az <235979@edu.p.lodz.pl>
2023-03-25 11:39:41 +0100

    stworzenie FiguresJaneAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/FiguresJaneAdapter.java
@@ -0,0 +1,15 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.powp.jobs2d.AbstractDriver;
+
+public class FiguresJaneAdapter extends AbstractDriver {
+
+    public FiguresJaneAdapter(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+}
