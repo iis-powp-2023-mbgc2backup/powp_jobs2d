21e3eb36b569f591cfc64818b446e8b2d6c70fe7
moginski01 <235936@edu.p.lodz.pl>
2023-03-27 11:45:40 +0200

    final refactor to remove unnecessary imports

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,11 +3,8 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
-
-import javax.swing.*;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
