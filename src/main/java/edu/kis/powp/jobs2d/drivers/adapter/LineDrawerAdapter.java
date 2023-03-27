6ac84a0ba09e8b9635dde10ac90de8164c8bd419
DMisztal <dominikmisztal@onet.eu>
2023-03-27 19:27:38 +0200

    Implemented line selection

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -10,10 +10,12 @@ public class LineDrawerAdapter extends DrawPanelController implements Job2dDrive
 	private int startX = 0, startY = 0;
 
 	private final DrawPanelController dpc;
+	private final int lineType;
 
-	public LineDrawerAdapter(DrawPanelController dpc) {
+	public LineDrawerAdapter(DrawPanelController dpc, int lineType) {
 		super();
 		this.dpc = dpc;
+		this.lineType = lineType;
 	}
 
 	@Override
@@ -24,7 +26,16 @@ public class LineDrawerAdapter extends DrawPanelController implements Job2dDrive
 
 	@Override
 	public void operateTo(int x, int y) {
-		ILine line = LineFactory.getSpecialLine();
+		ILine line;
+		if(lineType == 1){
+			line = LineFactory.getBasicLine();
+		}
+		else if(lineType == 2){
+			line = LineFactory.getDottedLine();
+		} else{
+			line = LineFactory.getSpecialLine();
+		}
+		
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
