d7d0327711c0fd62f17f3eefdb936eb5f56536ae
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-03-28 12:44:29 +0200

    add private atributes to classes

--- src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -2,6 +2,8 @@ package edu.kis.powp.jobs2d.commands;
 
 public class OperateToCommand implements DriverCommand{
 
+    private int x = 0, y = 0;
+
     @Override
     public void execute() {
     }
--- src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -1,7 +1,8 @@
 package edu.kis.powp.jobs2d.commands;
 
 public class SetPositionCommand implements DriverCommand{
-
+    private int x = 0, y = 0;
+    
     @Override
     public void execute() {
     }
