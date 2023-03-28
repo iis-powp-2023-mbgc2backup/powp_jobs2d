51343ee13528175c99f34a418652c27865aeb11c
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-03-28 13:12:59 +0200

    fix ComplexCommand

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -1,7 +1,22 @@
 package edu.kis.powp.jobs2d.commands;
 
-public class ComplexCommand {
-    public void storeAndExecute(DriverCommand driverCommand) {
-        driverCommand.execute();
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+    private List<Job2dDriver> drivers;
+
+    public ComplexCommand(List<Job2dDriver> drivers) {
+        this.drivers = drivers;
+    }
+
+    public void addDriver(Job2dDriver driver){
+        drivers.add(driver);
+    }
+
+    @Override
+    public void execute() {
+
     }
 }
