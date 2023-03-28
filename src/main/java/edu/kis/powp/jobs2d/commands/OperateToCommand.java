e46c7f989b4c96e6755a6e7e132b5546c2a9390b
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-03-28 12:43:04 +0200

    add classes which implements DriverCommand

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.commands;
+
+public class OperateToCommand implements DriverCommand{
+
+    @Override
+    public void execute() {
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.commands;
+
+public class SetPositionCommand implements DriverCommand{
+
+    @Override
+    public void execute() {
+    }
+}
