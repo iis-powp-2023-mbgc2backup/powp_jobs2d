4a48aa5e543bea8a647d464957593f69cdfa1468
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-03-28 12:47:19 +0200

    add constructors

--- src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -4,6 +4,11 @@ public class OperateToCommand implements DriverCommand{
 
     private int x = 0, y = 0;
 
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute() {
     }
--- src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -2,7 +2,12 @@ package edu.kis.powp.jobs2d.commands;
 
 public class SetPositionCommand implements DriverCommand{
     private int x = 0, y = 0;
-    
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public void execute() {
     }
