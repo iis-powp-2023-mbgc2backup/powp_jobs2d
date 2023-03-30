0a08102d8377fd12c47fecf68160543c1799c2ad
Ewangelina <103572361+Ewangelina@users.noreply.github.com>
2023-03-30 12:46:01 +0200

    Initial creation of new classes

---
+++ src/main/java/edu/kis/powp/jobs2d/DriverCommand.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.kis.powp.jobs2d;
+
+/**
+ *
+ * @author student
+ */
+public interface DriverCommand extends Job2dDriver
+{
+    public void execute();
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/SetPositionCommand.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.kis.powp.jobs2d;
+
+/**
+ *
+ * @author student
+ */
+public class SetPositionCommand implements DriverCommand
+{
+    private int x;
+    private int y;
+    
+   @Override
+   public void execute()
+   {
+       throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSyst
+   }
+
+    @Override
+    public void setPosition(int x, int y) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void operateTo(int x, int y) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   
+   
+}
