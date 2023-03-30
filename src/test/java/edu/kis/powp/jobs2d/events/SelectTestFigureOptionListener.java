c0945c36bb496a7ee9bd5e20f655e9b0357cac53
Kasia Jankowska <65082704+kasia251011@users.noreply.github.com>
2023-03-30 13:41:06 +0200

    Make constants

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -9,6 +9,8 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+	private final FIGURE_JOE_1 = "Figure Joe 1";
+	private final FIGURE_JOE_2 = "Figure Joe 2";
 
 	public SelectTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
@@ -17,8 +19,8 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-			case "Figure Joe 1" -> FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-			case "Figure Joe 2" -> FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+			case FIGURE_JOE_1 -> FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+			case FIGURE_JOE_2 -> FiguresJoe.figureScript2(driverManager.getCurrentDriver());
 		}
 	}
 }
