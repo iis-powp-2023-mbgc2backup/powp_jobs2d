5d03fca8d12c95bcd1876c2ca2742947058a3e6d
Mateusz Blaszczynski <blaszczu3@gmail.com>
2023-03-30 13:40:35 +0200

    Zadanie 1 i 2

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,23 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand{
+    private int x;
+    private int y;
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        driver.operateTo(x,y);
+    }
+
+    @Override
+    public void setPosition(int i, int i1) {
+
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+}
