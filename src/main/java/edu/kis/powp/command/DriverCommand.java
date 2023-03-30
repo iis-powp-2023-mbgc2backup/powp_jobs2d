ddd356bdecdcec7fdfa88d77a43b9f9e4f7397f7
SzymonB5 <235831@edu.p.lodz.pl>
2023-03-30 13:26:13 +0200

    Dodanie klasy testowej

--- src/main/java/edu/kis/powp/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/command/DriverCommand.java
@@ -1,23 +1,54 @@
 package edu.kis.powp.command;
+import edu.kis.powp.jobs2d.Job2dDriver;
 
-public interface DriverCommand {
+import java.util.ArrayList;
+
+// public interface Job2dDriver {
+//    void setPosition(int var1, int var2);
+//
+//    void operateTo(int var1, int var2);
+//}
+
+
+interface DriverCommand {
     void execute();
 }
 
 class SetPositionCommand implements DriverCommand {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
+
     @Override
     public void execute() {
 
     }
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 class OperateToCommand implements DriverCommand {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
+
     @Override
     public void execute() {
 
+    }
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+class ComplexCommand {
+    ArrayList<DriverCommand> driverCommands;
+
+    public ComplexCommand() {
+        this.driverCommands = new ArrayList<>();
     }
 }
