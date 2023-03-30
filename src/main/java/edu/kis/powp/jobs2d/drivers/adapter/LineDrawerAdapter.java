f0985f7f84e9ed354246d79b93dd3dca3cb7795c
Jakub Warda <236004@edu.p.lodz.pl>
2023-03-30 19:41:53 +0200

    dodanie pustej klasy Adaptera dla linii

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,15 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class LineDrawerAdapter implements Job2dDriver{
+    @Override
+    public void setPosition(int x, int y) {
+
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+
+    }
+}
