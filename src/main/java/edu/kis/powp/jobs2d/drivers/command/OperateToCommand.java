ed8c8f93f24f226e07cb86d4fd5a3c0ef515db6c
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-03-30 13:31:38 +0200

    Add classes which implements DriverCommand
    SetPositionCommand allows us to set position on driver
    OperateToCommand allows us to operate to position

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,2 +1,19 @@
-package edu.kis.powp.jobs2d.drivers.command;public class OperateToCommand {
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+
+    private int x;
+    private int y;
+
+    public OperateToCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver){
+        driver.operateTo(x,y);
+    }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -1,2 +1,19 @@
-package edu.kis.powp.jobs2d.drivers.command;public class SetPositionCommand {
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    SetPositionCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver){
+        driver.setPosition(x,y);
+    }
+
 }
