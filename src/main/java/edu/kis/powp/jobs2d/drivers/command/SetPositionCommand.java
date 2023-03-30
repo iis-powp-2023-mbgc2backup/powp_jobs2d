0403c5cdf19cacc192b2eb21d1959d43a13a2209
Kamil Sasin <ksasin3@gmail.com>
2023-03-30 12:58:54 +0200

    Implement class hierarchy from 3.1.1

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand implements DriverCommand {
+
+
+    private int x;
+    private int y;
+
+    public SetPositionCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
