abd119099ea283c6d585ed8344b2d0e2d8f13548
KamilCiepluch <235845@edu.p.lodz.pl>
2023-03-30 13:20:10 +0200

    Change classes

--- src/main/java/command/OperateToCommand.java
+++ src/main/java/command/OperateToCommand.java
@@ -6,18 +6,15 @@ public class OperateToCommand implements DriverCommand {
 
 	private final int x;
 	private final int y;
-	private final Job2dDriver driver;
 
-
-	public OperateToCommand(int x, int y, Job2dDriver driver)
+	public OperateToCommand(int x, int y )
 	{
 		this.x = x;
 		this.y = y;
-		this.driver = driver;
 	}
 
 	@Override
-	public void execute() {
+	public void execute(Job2dDriver driver) {
 		driver.operateTo(this.x,this.y);
 	}
 }
--- src/main/java/command/SetPositionCommand.java
+++ src/main/java/command/SetPositionCommand.java
@@ -5,16 +5,14 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class SetPositionCommand implements DriverCommand{
 	private final int x;
 	private final int y;
-	private final Job2dDriver driver;
 
-	public SetPositionCommand(int x, int y, Job2dDriver driver)
+	public SetPositionCommand(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
-		this.driver = driver;
 	}
 	@Override
-	public void execute() {
+	public void execute(Job2dDriver driver) {
 		driver.setPosition(this.x, this.y);
 	}
 }
