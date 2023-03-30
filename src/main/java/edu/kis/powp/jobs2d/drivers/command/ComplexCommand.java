af406986f92434b2c4f87db3a88e0a76f67f280c
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-03-30 13:33:41 +0200

    Add ComplexCommand
    ComplexCommand allows us to run list of commands on driver

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
@@ -1,2 +1,23 @@
-package edu.kis.powp.jobs2d.drivers.command;public class ComplexCommand {
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.ArrayList;
+
+public class ComplexCommand implements DriverCommand{
+    private ArrayList<DriverCommand> queueCommand;
+    public ComplexCommand(){
+        queueCommand = new ArrayList<>();
+    }
+    @Override
+    public void execute(Job2dDriver driver){
+        for(DriverCommand command : queueCommand){
+            command.execute(driver);
+        }
+    }
+
+    public void addCommand(DriverCommand command){
+        queueCommand.add(command);
+    }
+
 }
