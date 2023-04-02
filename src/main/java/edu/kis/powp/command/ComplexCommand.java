68cc80ac6e4967ee1c8d455276b79062111862c9
DMisztal <dominikmisztal@onet.eu>
2023-04-02 11:20:10 +0200

    Implemented complex command

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,24 @@
+package edu.kis.powp.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ComplexCommand implements DriverCommand{
+    private List<DriverCommand> commands;
+
+    public ComplexCommand(){
+        commands = new ArrayList<>();
+    }
+
+    public void addCommand(DriverCommand command){
+        commands.add(command);
+    }
+
+    @Override
+    public void execute(){
+        for (DriverCommand command : commands) {
+            command.execute();
+        }
+    }
+}
