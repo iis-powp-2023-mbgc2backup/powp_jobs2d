f10b739833b5e36daeaf7a83de21270af409b94b
Martyna <235887@edu.p.lodz.pl>
2023-04-03 22:25:36 +0200

    Trying to implement SquareFactory

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/ComplexCommand.java
@@ -1,9 +1,14 @@
 package edu.kis.powp.jobs2d.drivers.command;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
 
     private List<DriverCommand> commands;
+
+    public ComplexCommand() {
+        this.commands = new ArrayList<>();
+    }
 
     public ComplexCommand(List<DriverCommand> commands) {
         this.commands = commands;
