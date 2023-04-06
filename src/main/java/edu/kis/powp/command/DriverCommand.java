c79923ab2b24044a4ccc6cb7272fd1a692f7fd0c
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-06 17:06:39 +0200

    Poprawka klasy ComplexCommand

--- src/main/java/edu/kis/powp/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/command/DriverCommand.java
@@ -41,10 +41,22 @@ class OperateToCommand implements DriverCommand {
 
 }
 
-class ComplexCommand {
+class ComplexCommand implements DriverCommand {
     ArrayList<DriverCommand> driverCommands;
 
     public ComplexCommand() {
         this.driverCommands = new ArrayList<>();
     }
+
+    public void addCommand(DriverCommand command) {
+        this.driverCommands.add(command);
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        for (DriverCommand command : this.driverCommands) {
+            command.execute(driver);
+        }
+    }
+
 }
