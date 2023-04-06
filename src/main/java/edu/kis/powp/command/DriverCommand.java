9dd0a11a948f9a0ef697fba35307afbd77d435bb
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-06 16:07:05 +0200

    Poprawka klas

--- src/main/java/edu/kis/powp/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/command/DriverCommand.java
@@ -1,24 +1,27 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 import java.util.ArrayList;
 
 
 interface DriverCommand {
-    void execute();
+    void execute(Job2dDriver job2dDriver);
 }
 
 class SetPositionCommand implements DriverCommand {
     private final int x;
     private final int y;
 
-    @Override
-    public void execute() {
 
-    }
-
-    public SetPositionCommand(int x, int y) {
+    public SetPositionCommand(int x, int y, Job2dDriver job2dDriver) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(this.x, this.y);
     }
 }
 
@@ -26,14 +29,14 @@ class OperateToCommand implements DriverCommand {
     private final int x;
     private final int y;
 
-    @Override
-    public void execute() {
-
-    }
-
     public OperateToCommand(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(this.x, this.y);
     }
 
 }
