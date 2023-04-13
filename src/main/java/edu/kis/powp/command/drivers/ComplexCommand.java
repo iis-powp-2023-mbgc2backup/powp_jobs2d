55c08e025941145c7f77070bb8bf718a92664072
MateuszAdamski7 <235823@edu.p.lodz.pl>
2023-04-13 13:27:18 +0200

    Implement ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/drivers/ComplexCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.command.drivers;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+    private List<DriverCommand> driverCommands;
+
+    public ComplexCommand(List<DriverCommand> driverCommands){
+        this.driverCommands = driverCommands;
+    }
+
+    @Override
+    public void execute() {
+        for(DriverCommand command : driverCommands){
+            command.execute();
+        }
+    }
+}
