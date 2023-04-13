c1c4aa4c1e2f1cd8edf886ee7b63949a430e92fd
Barstok <mrbarstok@gmail.com>
2023-04-13 13:16:07 +0200

    ComplexCommand implementation

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -1,2 +1,19 @@
-package edu.kis.powp.jobs2d.commands;public class ComplexCommand {
+package edu.kis.powp.jobs2d.commands;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+
+    private List<DriverCommand> commands;
+
+    public ComplexCommand(List<DriverCommand> commands){
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute() {
+        for(DriverCommand command: commands){
+            command.execute();
+        }
+    }
 }
