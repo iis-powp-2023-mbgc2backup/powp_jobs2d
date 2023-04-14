054719c4e51288033cf86484e3f5aabe84463417
Jakub Warda <236004@edu.p.lodz.pl>
2023-04-14 16:11:40 +0200

    dodanie fabryki complex command

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
@@ -11,12 +11,20 @@ public class ComplexCommandFactory {
         List<DriverCommand> commmands=new ArrayList<DriverCommand>();
         commmands.add(new SetPositionCommand(driver,x,y));
         commmands.add(new OperateToCommand(driver,x+width,y));
-        
+        commmands.add(new OperateToCommand(driver,x-width/2,y+height));
+        commmands.add(new OperateToCommand(driver,x-width/2,y-height));
         ComplexCommand figure=new ComplexCommand(commmands);
-        return null;
+        return figure;
     }
-    public ComplexCommand createRectangle()
+    public ComplexCommand createRectangle(Job2dDriver driver,int x,int y,int width,int height)
     {
-        return null;
+        List<DriverCommand> commmands=new ArrayList<DriverCommand>();
+        commmands.add(new SetPositionCommand(driver,x,y));
+        commmands.add(new OperateToCommand(driver,x+width,y));
+        commmands.add(new OperateToCommand(driver,x,y+height));
+        commmands.add(new OperateToCommand(driver,x-width,y));
+        commmands.add(new OperateToCommand(driver,x,y-height));
+        ComplexCommand figure=new ComplexCommand(commmands);
+        return figure;
     }
 }
