a8bc9e5b020ee002fdae5214170abd80f0a56492
Jan Stasiak <236170@edu.p.lodz.pl>
2023-04-19 21:10:00 +0200

    Przetestowanie ComplexCommand

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,6 +3,9 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.command.ComplexCommand;
+import edu.kis.powp.command.OperateToCommand;
+import edu.kis.powp.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
@@ -19,6 +22,15 @@ public class SelectTestFigureOptionListener implements ActionListener {
 		switch (e.getActionCommand()) {
 			case "Figure Joe 1" -> FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 			case "Figure Joe 2" -> FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+			case "My test" -> {
+				ComplexCommand complexCommand = new ComplexCommand();
+				complexCommand.addCommand(new SetPositionCommand(120, 120, driverManager.getCurrentDriver()));
+				complexCommand.addCommand(new OperateToCommand(-120, 120, driverManager.getCurrentDriver()));
+				complexCommand.addCommand(new OperateToCommand(-120, -120, driverManager.getCurrentDriver()));
+				complexCommand.addCommand(new OperateToCommand(120, -120, driverManager.getCurrentDriver()));
+				complexCommand.addCommand(new OperateToCommand(120, 120, driverManager.getCurrentDriver()));
+				complexCommand.execute();
+			}
 		}
 	}
 }
