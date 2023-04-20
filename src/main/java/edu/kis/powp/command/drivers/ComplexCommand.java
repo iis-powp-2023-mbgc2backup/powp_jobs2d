5ed753c6451301a813809142895ddd9748686874
MateuszAdamski <235823@edu.p.lodz.pl>
2023-04-20 17:24:33 +0200

    fix ComplexCommand

--- src/main/java/edu/kis/powp/command/drivers/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/drivers/ComplexCommand.java
@@ -1,12 +1,17 @@
 package edu.kis.powp.command.drivers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
-    private List<DriverCommand> driverCommands;
+    private List<DriverCommand> driverCommands = new ArrayList<>();
 
     public ComplexCommand(List<DriverCommand> driverCommands){
         this.driverCommands = driverCommands;
+    }
+
+    public void addCommand(DriverCommand driverCommand){
+        driverCommands.add(driverCommand);
     }
 
     @Override
