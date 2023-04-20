f293ff244045db7cf1b77fc891241c8d83b31861
Czapi12344 <236013@edu.p.lodz.pl>
2023-04-20 13:13:36 +0200

    Fix operteTo

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -38,7 +38,7 @@ public class LineDrawerAdapter  implements Job2dDriver {
 	public void operateTo(int x, int y) {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		setPosition(x,y);
+		this.setPosition(x,y);
 		this.drawPanelController.drawLine(line);
 	}
 
