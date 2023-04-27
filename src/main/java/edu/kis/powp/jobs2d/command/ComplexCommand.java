bade35f0387dc5cb6a7c53aac07c9385189422b7
vor-o-na <78507829+vor-o-na@users.noreply.github.com>
2023-04-27 02:37:54 +0200

    create command classes; edit execute();

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -1,23 +1,24 @@
 package edu.kis.powp.jobs2d.command;
-
 import edu.kis.powp.jobs2d.Job2dDriver;
-
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
     private List<DriverCommand> commands;
+//    Job2dDriver driver;
 
     public ComplexCommand(List<DriverCommand> commands) {
+//    public ComplexCommand(List<DriverCommand> commands, Job2dDriver driver) {
+//        this.driver=driver;
         this.commands = commands;
     }
 
 
     @Override
-    public void execute(Job2dDriver driver) {
+    public void execute() {
         for (DriverCommand command : commands) {
-            command.execute(driver);
+            command.execute();
         }
     }
 
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -2,12 +2,19 @@ package edu.kis.powp.jobs2d;
 
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.command.ComplexCommand;
+import edu.kis.powp.jobs2d.command.DrawSmallSquareCommand;
+import edu.kis.powp.jobs2d.command.DrawSquareCommand;
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.MyLineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
@@ -29,6 +36,14 @@ public class TestJobs2dPatterns {
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
 		application.addTest("Figure Joe 2", selectTestFigureOptionListener);
+
+		List<DriverCommand> commands=new ArrayList<>();
+		DrawSquareCommand drawSquareCommand = new DrawSquareCommand(DriverFeature.getDriverManager().getCurrentDriver());
+		DrawSmallSquareCommand drawSmallSquareCommand = new DrawSmallSquareCommand(DriverFeature.getDriverManager().getCurrentDriver());
+		commands.add(drawSquareCommand);
+		commands.add(drawSmallSquareCommand);
+		ComplexCommand complexCommand = new ComplexCommand(commands);
+		//todo execute
 	}
 
 	/**
