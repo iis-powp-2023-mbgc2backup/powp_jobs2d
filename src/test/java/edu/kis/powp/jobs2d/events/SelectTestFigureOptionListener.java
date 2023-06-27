65e6f0a87746225f1b7917127dc9b5dccb804380
KamilS <ksasin3@gmail.com>
2023-06-27 16:47:23 +0200

    Extend actionPerform method

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,7 +3,10 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.fabrics.FigureFactory;
+import edu.kis.powp.jobs2d.features.DriverFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -16,6 +19,26 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		DriverCommand command;
+		switch (e.getActionCommand()) {
+			case "Figure Joe 1":
+				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+				break;
+			case "Figure Joe 2":
+				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+			case "Figure Square":
+				command = FigureFactory.getSquare();
+				command.execute(driverManager.getCurrentDriver());
+				break;
+			case "Figure Rectangle":
+				command = FigureFactory.getRectangle();
+				command.execute(driverManager.getCurrentDriver());
+				break;
+			case "Figure Triangle":
+				command = FigureFactory.getTriangle();
+				command.execute(driverManager.getCurrentDriver());
+				break;
+		}
 	}
 }
